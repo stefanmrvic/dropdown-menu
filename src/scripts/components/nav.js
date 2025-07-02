969d020922a9731cdb49f7a91a7379c012b844d1
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-07-02 11:26:20 +0200

    add: aria labels when dropdown is being clicked + reformat setTimeout logic for setting display: none in nav.js

--- src/scripts/components/nav.js
+++ src/scripts/components/nav.js
@@ -3,35 +3,30 @@ const profile = document.querySelector('.profile');
 profile.addEventListener('click', toggleDropDown);
 
 function toggleDropDown() {
-    const dropDownContainer = document.querySelector(
-        '.dropdown-nav__container'
-    );
-    const dropDownState = window.getComputedStyle(dropDownContainer).display;
+    const dropDownWrapper = document.querySelector('.dropdown-nav__wrapper');
 
-    if (dropDownState === 'none') expandDropDown();
-    else collapseDropDown();
+    dropDownWrapper.classList.toggle('open');
+    toggleAria();
 }
 
-function expandDropDown() {
+function toggleAria() {
+    const dropDownToggler = document.querySelector('.dropdown-toggler');
+    const dropDownTogglerAriaExpanded =
+        dropDownToggler.getAttribute('aria-expanded');
+
+    dropDownToggler.setAttribute(
+        'aria-expanded',
+        dropDownTogglerAriaExpanded === 'false' ? 'true' : 'false'
+    );
+
     const dropDownContainer = document.querySelector(
         '.dropdown-nav__container'
     );
+    const dropDownContainerAriaHidden =
+        dropDownContainer.getAttribute('aria-hidden');
 
-    dropDownContainer.style.display = 'flex';
-
-    setTimeout(() => {
-        dropDownContainer.classList.add('open');
-    }, 0);
-}
-
-function collapseDropDown() {
-    const dropDownContainer = document.querySelector(
-        '.dropdown-nav__container'
+    dropDownContainer.setAttribute(
+        'aria-hidden',
+        dropDownContainerAriaHidden === 'false' ? 'true' : 'false'
     );
-
-    dropDownContainer.classList.remove('open');
-
-    setTimeout(() => {
-        dropDownContainer.style.display = 'none';
-    }, 800);
 }
